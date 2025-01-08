d7ddbd30ba17a4064af2906773c4c6ef8654ae90
rubenaguilardev <ragui56@wgu.edu>
2025-01-08 00:58:37 -0800

    Add sound

--- js/main.js
+++ js/main.js
@@ -13,19 +13,25 @@ fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 document.querySelector('button').addEventListener('click', deal)
 
 function deal() {
+    let dealtCard = new Audio("sounds/carddrop.mp3")
+    let shuffle = new Audio("sounds/shuffle.mp3")
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            shuffle.play()
 
             setTimeout(() => {
-                document.querySelector('#playerCard1').src = data.cards[0].image}, 750)
+                dealtCard.play()
+                document.querySelector('#playerCard1').src = data.cards[0].image}, 850)
             setTimeout(() => {
-                document.querySelector('#playerCard2').src = data.cards[1].image}, 1500)
+                dealtCard.play()
+                document.querySelector('#playerCard2').src = data.cards[1].image}, 1600)
             setTimeout(() => {
-                document.querySelector('#dealerCard1').src = data.cards[2].image}, 2500)
+                dealtCard.play()
+                document.querySelector('#dealerCard1').src = data.cards[2].image}, 2600)
             setTimeout(() => {
-                document.querySelector('#dealerCard2').src = "img/back.png"}, 3500)
+                
+                document.querySelector('#dealerCard2').src = "img/back.png"}, 3600)
         })
         .catch(err => {
             console.log(`error ${err}`)
