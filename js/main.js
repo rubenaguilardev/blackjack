2ae2eea8fa17911ea890af78236262a104d18f60
rubenaguilardev <ragui56@wgu.edu>
2025-01-08 23:55:20 -0800

    Convert cards to numeric values

--- js/main.js
+++ js/main.js
@@ -16,7 +16,10 @@ function main() {
     document.querySelector('#play').addEventListener('click', deal)
 
     function deal() {
-         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+        let dealerCards = []
+        let dealerTotal = 0
+        let playerCards = []
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
             .then(res => res.json())
             .then(data => {
                 if (data.remaining === 52) {
@@ -26,16 +29,23 @@ function main() {
                 setTimeout(() => {
                     cardDealt()
                     document.querySelector('#playerCard1').src = data.cards[0].image}, 850)
+                    playerCards.push(data.cards[0].value)
                 setTimeout(() => {
                     cardDealt()
                     document.querySelector('#playerCard2').src = data.cards[1].image}, 1600)
+                    playerCards.push(data.cards[1].value)
                 setTimeout(() => {
                     cardDealt()
                     document.querySelector('#dealerCard1').src = data.cards[2].image}, 2600)
+                    dealerCards.push(data.cards[2].value)
                 setTimeout(() => {    
                     document.querySelector('#dealerCard2').src = "img/back.png"}, 3600)  
                 document.querySelector('#stand').addEventListener('click', function () {
                     document.querySelector('#dealerCard2').src = data.cards[3].image
+                    dealerCards.push(data.cards[3].value)
+                    console.log(dealerCards)
+                    dealerTotal = addCards(dealerCards)
+                    console.log(dealerTotal)
                 })      
         })
             .catch(err => {
@@ -54,9 +64,19 @@ function main() {
         shuffle.play()
     }
 
-    function playerStand(card) {
-        document.querySelector("#dealerCard2"). src = card
-    }
+    function addCards(cards) {
+        let total = 0
+        cards.forEach(card => {
+            if (card === 'ACE') {
+                total += 11
+            } else if (card === 'KING' || card === 'QUEEN' || card === 'JACK') {
+                total += 10
+            } else {
+                total += Number(card)
+            }
+        })
+        return total
+   }
 
    
 
