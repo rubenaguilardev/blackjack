0b4071e60c3d2e006d649e5ed927bf184e3b9f4b
rubenaguilardev <ragui56@wgu.edu>
2025-01-08 23:07:49 -0800

    Add buttons and more game logic

--- js/main.js
+++ js/main.js
@@ -1,39 +1,67 @@
 // Request a deck of cards from card API
-let deckId = ''
-fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then(res => res.json())
-    .then(data => {
-        deckId = data.deck_id
-        console.log(deckId)
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
+function main() {
 
-document.querySelector('button').addEventListener('click', deal)
-
-function deal() {
-    let dealtCard = new Audio("sounds/carddrop.mp3")
-    let shuffle = new Audio("sounds/shuffle.mp3")
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
+    let deckId = ''
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(res => res.json())
         .then(data => {
-            shuffle.play()
-
-            setTimeout(() => {
-                dealtCard.play()
-                document.querySelector('#playerCard1').src = data.cards[0].image}, 850)
-            setTimeout(() => {
-                dealtCard.play()
-                document.querySelector('#playerCard2').src = data.cards[1].image}, 1600)
-            setTimeout(() => {
-                dealtCard.play()
-                document.querySelector('#dealerCard1').src = data.cards[2].image}, 2600)
-            setTimeout(() => {
-                
-                document.querySelector('#dealerCard2').src = "img/back.png"}, 3600)
+            deckId = data.deck_id
+            console.log(deckId)
+            console.log(data)
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
-}
+
+    document.querySelector('#play').addEventListener('click', deal)
+
+    function deal() {
+         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.remaining === 52) {
+                    shuffle()
+                }
+                console.log(data)
+                setTimeout(() => {
+                    cardDealt()
+                    document.querySelector('#playerCard1').src = data.cards[0].image}, 850)
+                setTimeout(() => {
+                    cardDealt()
+                    document.querySelector('#playerCard2').src = data.cards[1].image}, 1600)
+                setTimeout(() => {
+                    cardDealt()
+                    document.querySelector('#dealerCard1').src = data.cards[2].image}, 2600)
+                setTimeout(() => {    
+                    document.querySelector('#dealerCard2').src = "img/back.png"}, 3600)  
+                document.querySelector('#stand').addEventListener('click', function () {
+                    document.querySelector('#dealerCard2').src = data.cards[3].image
+                })      
+        })
+            .catch(err => {
+                console.log(`error ${err}`)
+            })
+          
+    }
+
+    function cardDealt() {
+        let dealtCard = new Audio("sounds/carddrop.mp3")
+        dealtCard.play()
+    }
+
+    function shuffle() {
+        let shuffle = new Audio("sounds/shuffle.mp3")
+        shuffle.play()
+    }
+
+    function playerStand(card) {
+        document.querySelector("#dealerCard2"). src = card
+    }
+
+   
+
+    }
+
+
+
+main()
