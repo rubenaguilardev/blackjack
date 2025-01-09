aad099f6ca9cecab29594ab57b7b6443fa5f69e3
rubenaguilardev <ragui56@wgu.edu>
2025-01-09 15:49:43 -0800

    Add hit functionality

--- js/main.js
+++ js/main.js
@@ -16,37 +16,72 @@ function main() {
     document.querySelector('#play').addEventListener('click', deal)
 
     function deal() {
+        
         let dealerCards = []
         let dealerTotal = 0
         let playerCards = []
+        let playerTotal = 0
+
         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
             .then(res => res.json())
             .then(data => {
                 if (data.remaining === 52) {
                     shuffle()
                 }
-                console.log(data)
+
                 setTimeout(() => {
-                    cardDealt()
+                    cardDealtSound()
                     document.querySelector('#playerCard1').src = data.cards[0].image}, 850)
                     playerCards.push(data.cards[0].value)
                 setTimeout(() => {
-                    cardDealt()
+                    cardDealtSound()
                     document.querySelector('#playerCard2').src = data.cards[1].image}, 1600)
                     playerCards.push(data.cards[1].value)
                 setTimeout(() => {
-                    cardDealt()
+                    cardDealtSound()
                     document.querySelector('#dealerCard1').src = data.cards[2].image}, 2600)
                     dealerCards.push(data.cards[2].value)
                 setTimeout(() => {    
                     document.querySelector('#dealerCard2').src = "img/back.png"}, 3600)  
-                document.querySelector('#stand').addEventListener('click', function () {
-                    document.querySelector('#dealerCard2').src = data.cards[3].image
-                    dealerCards.push(data.cards[3].value)
-                    console.log(dealerCards)
-                    dealerTotal = addCards(dealerCards)
-                    console.log(dealerTotal)
-                })      
+
+                document.querySelector('#hit').addEventListener('click', function () {
+                    drawCard()
+                        .then(function (newCard) { 
+                            if (document.querySelector('.playerCards').contains(document.querySelector('.nextCard'))) {
+                                cardDealtSound()
+                                let newCardImage = createCardImage(newCard.image)
+                                let playerCardsContainer = document.querySelector('.playerCards')
+                                let numCards = playerCardsContainer.children.length
+                                let newLeft = numCards *65
+                                newCardImage.style.left = `${newLeft}px`
+                                playerCardsContainer.appendChild(newCardImage)
+                                playerCards.push(newCard.value)
+                                console.log(playerCards)
+                            } else {
+                                cardDealtSound()
+                                let newCardImage = createCardImage(newCard.image)
+                                document.querySelector('.playerCards').appendChild(newCardImage)  
+                                playerCards.push(newCard.value)
+                                console.log(playerCards)
+                            }
+                              
+                        })
+                        
+                })
+
+                // document.querySelector('#stand').addEventListener('click', function () {
+
+                //     document.querySelector('#dealerCard2').src = data.cards[3].image
+                //     dealerCards.push(data.cards[3].value)
+                //     playerTotal = addCardTotal(playerCards)
+                //     console.log(playerTotal)
+                //     dealerTotal = addCardTotal(dealerCards)
+                //     console.log(dealerTotal)
+                //     if (dealerTotal < 17) {
+                //         let card = drawCard()
+                //         console.log(card)
+                //     }
+                // })      
         })
             .catch(err => {
                 console.log(`error ${err}`)
@@ -54,17 +89,20 @@ function main() {
           
     }
 
-    function cardDealt() {
+
+    function cardDealtSound() {
         let dealtCard = new Audio("sounds/carddrop.mp3")
         dealtCard.play()
     }
+
 
     function shuffle() {
         let shuffle = new Audio("sounds/shuffle.mp3")
         shuffle.play()
     }
 
-    function addCards(cards) {
+
+    function addCardTotal(cards) {
         let total = 0
         cards.forEach(card => {
             if (card === 'ACE') {
@@ -78,9 +116,22 @@ function main() {
         return total
    }
 
-   
 
+    function drawCard() {
+        return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+            .then(res => res.json())
+            .then(data => {
+                return data.cards[0]
+            })
     }
+
+    function createCardImage(card) {
+        let newCardImage = document.createElement('img')
+        newCardImage.classList.add('nextCard')
+        newCardImage.src = card
+        return newCardImage
+    }
+}
 
 
 
