5dce771604bf3da9f7b02dbd2119f498598a292e
Ruben Aguilar <ragui56@wgu.edu>
2026-01-09 03:35:57 -0800

    add background, title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 const App = () => {
   return (
-    <div className=""></div>
+    <div className="min-h-screen overflow-x-hidden">
+      <main className="min-h-screen bg-[url('/table.jpg')] bg-cover text-center py-10">
+        <h1 className="rey text-white text-5xl">Blackjack</h1>
+      </main>
+    </div>
   )
 }
 
