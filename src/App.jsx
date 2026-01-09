5feb3eec55f4171ba653508c2f9bed6708536c38
Ruben Aguilar <ragui56@wgu.edu>
2026-01-09 15:18:08 -0800

    replace title with logo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
-      <main className="min-h-screen bg-[url('/table.jpg')] bg-cover text-center py-10">
-        <h1 className="rey text-white text-5xl">Blackjack</h1>
+      <main className="min-h-screen bg-[url('/table.jpg')] bg-cover px-6 py-10">
+        <img src="/logo.png" alt="blackjack logo" />
       </main>
     </div>
   )
