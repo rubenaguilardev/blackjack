d5704534c1422704b08d5a0bd8a49c8c4fbcba72
Ruben Aguilar <ragui56@wgu.edu>
2026-01-10 04:50:21 -0800

    add get deck id api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
+   
+  const [game, setGame] = useState(
+    {
+      deckId: null,
+      player : {
+        status: 'active',
+        hand: [],
+        chipCount: 500,
+      },
+      dealerHand: [],
+      gamePhase: 'not started',
+      pot: 0
+    }
+  )
+
+ useEffect(() => {
+  fetch('https://deckofcardsapi.com/api/deck/new/')
+    .then(res => res.json())
+    .then(data => {
+      setGame(prev => ({
+        ...prev, 
+        deckId: data.deck_id
+      }))
+    })
+ }, [])
+  
+  
   return (
-    <div className="min-h-screen overflow-x-hidden">
-      <main className="min-h-screen bg-[url('/table.jpg')] bg-cover px-6 py-10">
+    <div className="min-h-screen overflow-x-hidden bg-[url('/table.jpg')] bg-cover">
+      <main className="container flex justify-center md:justify-start items-start mx-auto min-h-screen px-5 py-12">
         <img src="/logo.png" alt="blackjack logo" />
       </main>
     </div>
